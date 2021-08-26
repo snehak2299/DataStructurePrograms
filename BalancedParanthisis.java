c2b88da5cdc9965706bc8f22bf6c9c77c4d1d6da
snehak2299 <skanase2299@gmail.com>
2021-08-26 23:04:42 +0530

    [sneha]:program to check given expression is balanced or not

---
+++ BalancedParanthisis.java
@@ -0,0 +1,52 @@
+package com.bridglab.balancedparanthisis;
+
+import java.util.Scanner;
+
+public class BalancedParanthisis {
+
+	public static void main(String[] args) {
+		
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter max lenght: ");
+			int n=sc.nextInt();
+		
+			StackL stack = new StackL(n); //create stack
+			System.out.println();
+			 
+			System.out.println("Enter expression: "); // enter expression
+			String exp = sc.next();        
+			int len = exp.length();
+			System.out.println("check expression parantheses mathcing or not: ");
+
+			for (int i = 0; i < len; i++)
+			{    
+			char ch = exp.charAt(i);
+
+				if (ch == '(')
+				{
+				stack.push(i);
+				}
+					else if (ch == ')')
+					{
+						try
+						{
+						long p = (stack.pop() + 1);
+						System.out.println("')' at index "+(i+1)+" matched with '(' at index "+p);
+						System.out.println("True");
+						}
+						catch(Exception e)
+						{
+						System.out.println("')' at index "+(i+1)+" is unmatched");
+						System.out.println("False");
+						}
+					}            
+				}
+			while (!stack.isEmpty() )
+			{
+			System.out.println("'(' at index "+(stack.pop() +1)+" is unmatched");
+			}                          
+	    
+
+	}
+
+}
