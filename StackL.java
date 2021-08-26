7e268a48f91dea14ac9389219a1f784e31e0f9e0
snehak2299 <skanase2299@gmail.com>
2021-08-26 23:02:10 +0530

    [sneha]:stack logic is added

---
+++ StackL.java
@@ -0,0 +1,40 @@
+package com.bridglab.balancedparanthisis;
+
+public class StackL {
+	private int maxSize;
+	private long[] stackArray;
+	private int top;
+
+	//for size  
+	public StackL(int s)
+	{
+	maxSize = s;
+	stackArray = new long[maxSize];
+	top = -1;
+	}
+	//for push   
+	public void push(long j)
+	{
+	stackArray[++top] = j;
+	}
+	//for pop   
+	public long pop() 
+	{
+	return stackArray[top--];
+	}
+	//for peek   
+	public long peek() 
+	{
+	return stackArray[top];
+	}
+	//to check whether stack is empty	   
+	public boolean isEmpty()
+	{
+	return (top == -1);
+	}
+	//to check whether stack is full	   
+	public boolean isFull()
+	{
+	return (top == maxSize - 1);
+	}
+}
